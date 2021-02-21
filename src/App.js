a2b6cc3190206e912b5cab096298c800c6132d19
Chandan Kumar <descentchandan@gmail.com>
2021-02-21 15:24:34 +0530

    toggleVisibilty working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person'
+import Men from './men/men'
 import {React,Component} from "react"
+import "./Person/Person.css"
+
 class App extends Component {
    state={
      persons:[
      {name:"chan",age:30},
      {name:"akku",age:20},
      {name:"abhi",age:10},
-   ]}
+   ],
+    toggleName: false,
+    visibility:false
+  }
 
 
    nameSwitchHandler = (newName)=>{
@@ -30,6 +36,13 @@ class App extends Component {
         ]
      })
    }
+
+   toggleVisibility=()=>{
+     let visible= this.state.visibility
+      this.setState({
+        visibility: !visible
+      })
+   }
    
   render () {
 
@@ -38,19 +51,55 @@ class App extends Component {
       fontSize:"20px",
       padding:"10px"
     } 
-    
+
     return (
     <div className="App">
       <h1>Hello react</h1>
-      <button style={style} onClick={this.nameSwitchHandler.bind(this,"deepu")}>
+      <button style={style} onClick={this.toggleVisibility}>
         Switch
       </button>
-      <Person className="Per" click={this.nameSwitchHandler.bind(this,"kundan")} name={this.state.persons[0].name} age={this.state.persons[0].age} />
+     
+     {this.state.visibility ? <div>
+         <Person className="Per" click={this.nameSwitchHandler.bind(this,"kundan")} name={this.state.persons[0].name} age={this.state.persons[0].age} />
       <Person className="Per" changed={this.nameChangedHandler} name={this.state.persons[1].name} age={this.state.persons[1].age}> This is my hobby</Person>
       <Person className="Per" name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-    </div>
+     </div> : null}
+    </div> 
   );
 }
 }
+
+
+// class App extends Component{
+
+//   state={
+//     mens:[
+//       {name:"chan", age:10 },
+//       {name:'abhi', age:12 }
+//     ]
+//   }
+
+  
+//   changeNameHandler= (newname)=>{
+//     this.setState({
+//        mens:[
+//       {name:"chan new ", age:10 },
+//       {name:newname, age:12 }
+//     ]
+//     })
+//   }
+
+//   render(){
+//     let style={
+//        backgroundColor:"red"
+//      }
+//     return (
+//       <div className="App">
+//         <button className="person" style={style} onClick={this.changeNameHandler.bind(this,"akk")}  > switch</button>
+//         <Men name={this.state.mens[1].name} age={this.state.mens[1].age} />
+//       </div>
+//     )
+//   }
+// }
 
 export default App
